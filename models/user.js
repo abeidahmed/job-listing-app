71462ea0b52a27d4a9d60b6cf391cb40902c7258
abeidahmed92@gmail.com <abeidahmed92@gmail.com>
2020-04-11 19:29:04 +0530

    Install dependencies and add email validation

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,7 +18,12 @@ const userSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       lowercase: true,
-      required: true
+      required: true,
+      validate(email) {
+        if (!validator.isEmail(email)) {
+          throw new Error("Invalid email address.");
+        }
+      }
     },
     password: {
       type: String,
