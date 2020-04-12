2f55fb62a0f196e1b3dc3732b12f227324900692
abeidahmed <abeidahmed92@gmail.com>
2020-04-12 17:49:00 +0530

    Configure initial routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,55 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyle } from "./theme";
+import PublicRouter from "./routes/publicRouter";
 
-// test
-import { H1, H2, H3 } from "./components/typography";
-import { StyledButton } from "./components/button";
-import Icon from "./components/icon";
-import { InputField } from "./components/field";
-
-function App() {
+const Routes = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className="App">
-        <H1 color="dark" size="2xl">
-          Hello world
-        </H1>
-        <H2 color="gray" size="xl">
-          Hello world
-        </H2>
-        <H3 color="light" size="lg">
-          Hello world
-        </H3>
-        <br />
-        <br />
-        <br />
-        <StyledButton type="primary" size="sm" icon>
-          <Icon color="red" glyph="briefcase" height="16" width="16" />
-          hello worlds
-        </StyledButton>
-        <br />
-        <br />
-        <div style={{ display: "flex" }}>
-          <StyledButton type="bordered" icon>
-            <Icon color="red" glyph="dollar" height="20" width="20" />
-            Button
-          </StyledButton>
-          <InputField type="primary" placeholder="Search..." />
-        </div>
-        <StyledButton type="bordered" iconOnly>
-          <Icon color="red" glyph="dollar" height="20" width="20" />
-        </StyledButton>
-        <div style={{ display: "flex" }}>
-          <StyledButton type="bordered" size="sm" isLink>
-            <a href="https://google.com">hello world</a>
-          </StyledButton>
-          <InputField size="sm" type="danger" placeholder="Search..." />
-        </div>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" component={PublicRouter} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
-}
+};
 
-export default App;
+export default Routes;
---
+++ client/src/routes/publicRouter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "../views/home";
+import Register from "../views/register";
+
+const PublicRouter = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/sign_up" component={Register} />
+    </Switch>
+  );
+};
+
+export default PublicRouter;
