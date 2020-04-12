003bf8494f4a9d537ab182fc04f1f3bc3745f96c
abeidahmed <abeidahmed92@gmail.com>
2020-04-12 18:34:29 +0530

    Add breakpoint and container layout

---
+++ client/src/components/layout/container.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { breakpoint } from "../../utils/breakpoint";
+
+export const Container = styled.div`
+  padding-left: 24px;
+  padding-right: 24px;
+  margin-left: auto;
+  margin-right: auto;
+
+  ${breakpoint.sm`
+    max-width: 576px;
+  `}
+
+  ${breakpoint.md`
+    max-width: 768px;
+  `}
+
+  ${breakpoint.lg`
+    max-width: 992px;
+  `}
+
+  ${breakpoint.xl`
+    max-width: 1170px;
+  `}
+`;
