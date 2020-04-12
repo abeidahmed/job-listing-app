461db87cfbe737f6b96ddd6294f7051d403778e9
abeidahmed <abeidahmed92@gmail.com>
2020-04-12 22:25:17 +0530

    Setup login page

--- client/src/routes/publicRouter.js
+++ client/src/routes/publicRouter.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "views/home";
+import Login from "views/login";
 import Register from "views/register";
 
 const PublicRouter = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
       <Route exact path="/sign_up" component={Register} />
     </Switch>
   );
---
+++ client/src/views/login/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>hello from login</h1>
+    </div>
+  );
+};
+
+export default Login;
