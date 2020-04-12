fa21790b566cb091b706a609ec0805157af2ba33
abeidahmed92@gmail.com <abeidahmed92@gmail.com>
2020-04-12 10:50:26 +0530

    Add figma variables

--- client/src/theme/variables.js
+++ client/src/theme/variables.js
@@ -1,51 +1,28 @@
 export const theme = {
   color: {
-    gray50: "#f9fafb",
+    gray50: "#F5F7FA",
     gray100: "#F7FAFC",
-    gray200: "#e5e7eb",
-    gray300: "#d2d6dc",
-    gray400: "#9fa6b2",
-    gray500: "#6b7280",
-    gray600: "#4b5563",
+    gray200: "#E2E2EA",
+    gray300: "#D2D6DC",
+    gray400: "#9FA6B2",
+    gray500: "#6B7280",
+    gray600: "#4B5563",
     gray700: "#374151",
-    gray800: "#252f3f",
+    gray800: "#171725",
     red300: "#FEB2B2",
     red600: "#E53E3E",
     red700: "#C53030",
     red800: "#9B2C2C",
     blue600: "#0069ff",
     blue700: "#1633ff",
-    blue800: "#031B4E",
-    blue900: "#00142F",
-    blue1000: "#000A18"
+    textDark: "#031B4E",
+    textGray: "rgba(3,27,78,.7)",
+    textLight: "rgba(3,27,78,.5)"
   },
   shadow: {
-    inset: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
-    small: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+    small: "0 1px 3px 0 rgba(0, 0, 0, 0.1) , 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
     medium: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
-    large: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
     outline: "0 0 0 3px rgba(66, 153, 225, 0.35)",
     outlineError: "0 0 0 3px rgba(229, 62, 62, 0.35)"
-  },
-  tracking: {
-    wide: "0.025em",
-    wider: "0.05em",
-    widest: "0.1em"
-  },
-  leading: {
-    none: "1",
-    tight: "1.25",
-    snug: "1.375",
-    normal: "1.5",
-    relaxed: "1.625",
-    loose: "2",
-    "3": ".75rem",
-    "4": "1rem",
-    "5": "1.25rem",
-    "6": "1.5rem",
-    "7": "1.75rem",
-    "8": "2rem",
-    "9": "2.25rem",
-    "10": "2.5rem"
   }
 };
