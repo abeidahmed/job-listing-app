3796a8f2bda64c818ab773896c4c0d069a7906f5
abeidahmed <abeidahmed92@gmail.com>
2020-04-13 14:00:33 +0530

    Create a working login form

--- client/src/views/login/components/form.js
+++ client/src/views/login/components/form.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { StyledButton } from "components/button";
 import { FormWrapper, Label, InputWrapper, StyledPara } from "./style";
 
 const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const postUserData = () => {
+    axios
+      .post(
+        "/api/v1/login",
+        {
+          email,
+          password
+        },
+        { withCredentials: true }
+      )
+      .then(res => {
+        if (res.status === 200) {
+          console.log(res);
+        }
+      })
+      .catch(err => console.log(err));
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    await postUserData();
+  };
+
   return (
-    <FormWrapper>
+    <FormWrapper onSubmit={handleSubmit}>
       <div>
         <Label htmlFor="email__login">Email address</Label>
-        <InputWrapper id="email__login" type="email" />
+        <InputWrapper id="email__login" type="email" value={email} onChange={e => setEmail(e.target.value)} />
       </div>
       <div>
         <Label htmlFor="password__login">Password</Label>
-        <InputWrapper id="password__login" type="password" />
+        <InputWrapper
+          id="password__login"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
       </div>
       <div>
         <StyledPara size="sm">
