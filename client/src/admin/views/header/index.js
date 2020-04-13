0ce8ebdf441d6a3afdd76686b3ee73f102af5fed
abeidahmed <abeidahmed92@gmail.com>
2020-04-14 01:03:15 +0530

    Change avatar image

--- client/src/admin/views/header/index.js
+++ client/src/admin/views/header/index.js
@@ -12,7 +12,7 @@ const Header = () => {
         <PillWithNumber glyph="bell" number="5" />
         <AvatarWithText
           text="Jennifer"
-          img="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          img="https://images.unsplash.com/photo-1492106087820-71f1a00d2b11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&&facepad=2.5&w=256&h=256&q=80"
         />
       </Nav>
     </StyledHeader>
