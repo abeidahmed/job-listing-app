19ba412390c8e6885a9e783991dd487840da0949
abeidahmed <abeidahmed92@gmail.com>
2020-04-15 14:57:06 +0530

    Enable redux track

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,5 +2,8 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    trace: true
+  }) || compose;
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
